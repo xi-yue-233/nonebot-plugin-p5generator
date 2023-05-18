d98281cac8846ab620082afb2223932901892273
xi-yue-233 <1004514855@qq.com>
2023-05-18 16:27:34 +0800

    新增功能-p5ui

--- nonebot_plugin_p5generator/__init__.py
+++ nonebot_plugin_p5generator/__init__.py
@@ -2,9 +2,10 @@ from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot.params import CommandArg
 
-from .generator import generate_image
+from .generator import generate_image,generate_ui
 
 p5=on_command('p5generator', aliases={'p5预告信'})
+p5_ui=on_command('p5ui', aliases={'p5风格'})
 
 @p5.handle()
 async def p5generator(args: Message = CommandArg()):
@@ -13,3 +14,9 @@ async def p5generator(args: Message = CommandArg()):
     image = await generate_image(args.extract_plain_text())
     await p5.finish(MessageSegment.image(image))
 
+@p5_ui.handle()
+async def p5generatorui(args: Message = CommandArg()):
+    if not args.extract_plain_text():
+        await p5.finish(f"给我要生成的语句啊，不然佐仓双叶来了都没办法生成")
+    image = await generate_ui(args.extract_plain_text())
+    await p5.finish(MessageSegment.image(image))
--- nonebot_plugin_p5generator/generator.py
+++ nonebot_plugin_p5generator/generator.py
@@ -10,7 +10,6 @@ import random
 RESOURCES_PATH = Path() / 'data' / 'p5generator'
 RESOURCES_PATH.mkdir(parents=True, exist_ok=True)
 
-
 async def generate_image(text):
     # 设置字体列表
     fonts = ['msyh.ttc', 'STHUPO.TTF', 'simsun.ttc', 'msyhbd.ttc', 'simhei.ttf']
@@ -84,6 +83,99 @@ async def generate_image(text):
 
             # 绘制背景色块
             char_draw.rectangle((0, 0, char_width, char_height), fill=bg_color)
+            # 绘制字符
+            char_draw.text((0, 0), char, fill=color, font=font)
+
+            # 随机旋转角度
+            angle = random.randint(-5, 5)
+            char_image = char_image.rotate(angle, resample=Image.BICUBIC, expand=True)
+
+            # 将字符图像粘贴到背景图像上
+            background_image.paste(char_image, (x, y), char_image)
+
+            # 更新x坐标
+            x += char_width + 20
+
+            # 更新y坐标以换行
+        y += max_char_height + 20
+    # 返回图像
+    result = await convert_img(background_image)
+    return result
+
+
+async def generate_ui(text):
+    # 设置字体列表
+    fonts = ['msyhbd.ttc']
+    # 设置颜色列表
+    colors = ['white', 'red']
+    # 设置背景颜色列表
+    bg_colors = {'white': ['black', 'red'], 'red': ['black']}
+
+    # 打开背景图片
+    background_image = Image.open(os.path.join(RESOURCES_PATH, "background.png"))
+
+    # 获取背景图片大小
+    width, height = background_image.size
+
+    # 分割文本为多个句子
+    sentences = text.split('\n')
+
+    # 计算文本总高度
+    text_height = 0
+    max_char_height = 0
+    sentences_count = len(sentences)
+    for sentence in sentences:
+        sentence = sentence.replace('\r', "")
+        sentence = sentence.replace('\r\n', "")
+        text_size = min(1770 // len(sentence), 1300 // (sentences_count + 6))
+        for char in sentence:
+            # 随机选择字体和大小
+            font = ImageFont.truetype(os.path.join(RESOURCES_PATH, random.choice(fonts)),
+                                      random.randint(text_size, text_size + 20))
+            # 获取字符大小
+            char_width, char_height = font.getsize(char)
+            max_char_height = max(max_char_height, char_height)
+        text_height += max_char_height + 20
+
+    # 计算文本起始位置
+    y = (height - text_height) // 2
+
+    # 遍历每个句子
+    for sentence in sentences:
+        # 计算句子宽度
+        sentence = sentence.replace('\r', "")
+        sentence = sentence.replace('\r\n', "")
+        sentence_width = 0
+        text_size = min(1770 // (len(sentence) + 6), 1300 // (sentences_count + 6))
+        for char in sentence:
+            # 随机选择字体和大小
+            font = ImageFont.truetype(os.path.join(RESOURCES_PATH, random.choice(fonts)),
+                                      random.randint(text_size, text_size + 20))
+            # 获取字符大小
+            char_width, char_height = font.getsize(char)
+            sentence_width += char_width + 20
+
+        # 计算句子起始位置
+        x = (width - sentence_width) // 2
+
+        # 遍历句子中的每个字符
+        for index, char in enumerate(sentence):
+            # 随机选择字体和大小
+            font = ImageFont.truetype(os.path.join(RESOURCES_PATH, random.choice(fonts)),
+                                      random.randint(text_size, text_size + 20))
+            # 随机选择颜色
+            color = random.choice(colors)
+            # 根据颜色选择背景颜色
+            bg_color = random.choice(bg_colors[color])
+            # 获取字符大小
+            char_width, char_height = font.getsize(char)
+
+            # 创建字符图像和绘图对象
+            char_image = Image.new('RGBA', (char_width, char_height))
+            char_draw = ImageDraw.Draw(char_image)
+
+            # 绘制背景色块
+            char_draw.rectangle((0, 0, char_width+7, char_height+7), fill=bg_color, outline='white', width=7)
             # 绘制字符
             char_draw.text((0, 0), char, fill=color, font=font)
 
